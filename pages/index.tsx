6f3531e3d11be8c61a0ef39d517b957bf303d912
Marwan Fikri <marwan.fikri200@gmail.com>
2023-05-12 10:32:32 +0200

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,8 +105,6 @@ export default function HeroImageBackground() {
 
   return (
     <div className={classes.wrapper}>
-      <Overlay color="#000" opacity={0.65} zIndex={1} />
-
       <div className={classes.inner}>
         <Title className={classes.title}>
           Automated AI code reviews for{" "}
